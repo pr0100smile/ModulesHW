8bdc771a46a6911af6633898712f23e25ef95f94
Dmitriy NetoIT <pr0100_smile@mail.ru>
2024-10-15 22:27:39 +0300

    Initial commit

---
+++ src/js/game.js
@@ -0,0 +1,17 @@
+import { Character } from './domain.js';
+export { readGameSaving, writeGameSaving };
+
+export default class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+export class GameSavingData {
+}
+  
+function readGameSaving() {
+}
+  
+function writeGameSaving() {
+}
